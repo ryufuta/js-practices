11a1ee04a0b25efcf093e392aeb216b839c895f4
ryufuta <rfutago@gmail.com>
2024-10-17 16:34:15 +0900

    Change the order of variables to shorten their scope

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -14,8 +14,8 @@ const buildCalendarBody = (year, month) => {
   const saturday = 6;
   const firstDate = new Date(year, month - 1, 1);
   const lastDate = new Date(year, month, 0);
-  const rows = [];
   let datesPerWeek = new Array(firstDate.getDay()).fill("  ");
+  const rows = [];
   [...Array(lastDate.getDate())]
     .map((_, i) => new Date(year, month - 1, i + 1))
     .forEach((date) => {
