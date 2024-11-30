e9a0e5b9bce2b189efa8075ce69b0526d153cd59
ryufuta <rfutago@gmail.com>
2024-11-30 16:34:02 +0900

    Implement async/await version

---
+++ 03.asynchronous/async_await.js
@@ -0,0 +1,37 @@
+import sqlite3 from "sqlite3";
+import timers from "timers/promises";
+import { dbRunWithPromise, dbGetWithPromise } from "./lib/sqlite3_promise.js";
+
+const db = new sqlite3.Database(":memory:");
+
+// エラーなしのプログラム
+await dbRunWithPromise(db, "CREATE TABLE books (title TEXT NOT NULL UNIQUE)");
+const value = await dbRunWithPromise(
+  db,
+  "INSERT INTO books VALUES (?)",
+  "JavaScript Primer",
+);
+console.log(`自動採番されたID: ${value.lastID}`);
+const row = await dbGetWithPromise(db, "SELECT rowid AS id, title FROM books");
+console.log(`取得したレコード: id: ${row.id}, title: ${row.title}`);
+await dbRunWithPromise(db, "DROP TABLE books");
+
+await timers.setTimeout(100);
+
+// エラーありのプログラム
+await dbRunWithPromise(db, "CREATE TABLE books (title TEXT NOT NULL UNIQUE)");
+try {
+  await dbRunWithPromise(db, "INSERT INTO books VALUES (?)", null);
+} catch (error) {
+  console.error(`レコード追加時のエラー: ${error}`);
+}
+
+try {
+  await dbGetWithPromise(db, "SELECT content FROM books");
+} catch (error) {
+  console.error(`レコード取得時のエラー: ${error}`);
+}
+
+await dbRunWithPromise(db, "DROP TABLE books");
+
+db.close();
