c877f30d4d5d75cc012cdd913bfe642e87df28d5
ryufuta <rfutago@gmail.com>
2024-12-14 21:19:13 +0900

    Use arrow functions instead of `function` keyword when possible

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -4,7 +4,7 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
 // エラーなしのプログラム
-db.run("CREATE TABLE books (title TEXT NOT NULL UNIQUE)", function () {
+db.run("CREATE TABLE books (title TEXT NOT NULL UNIQUE)", () => {
   db.run("INSERT INTO books VALUES (?)", "JavaScript Primer", function () {
     console.log(`自動採番されたID: ${this.lastID}`);
     db.get("SELECT rowid AS id, title FROM books", (_, row) => {
@@ -17,12 +17,12 @@ db.run("CREATE TABLE books (title TEXT NOT NULL UNIQUE)", function () {
 await timers.setTimeout(100);
 
 // エラーありのプログラム
-db.run("CREATE TABLE books (title TEXT NOT NULL UNIQUE)", function () {
-  db.run("INSERT INTO books VALUES (?)", null, function (error) {
+db.run("CREATE TABLE books (title TEXT NOT NULL UNIQUE)", () => {
+  db.run("INSERT INTO books VALUES (?)", null, (error) => {
     console.error(`レコード追加時のエラー: ${error}`);
     db.get("SELECT content FROM books", (error) => {
       console.error(`レコード取得時のエラー: ${error}`);
-      db.run("DROP TABLE books", function () {
+      db.run("DROP TABLE books", () => {
         db.close();
       });
     });
