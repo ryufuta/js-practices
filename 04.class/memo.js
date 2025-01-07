63161db1a6ab3f7d7fd8a5f0fa5a18ecfc01b95d
ryufuta <rfutago@gmail.com>
2025-01-07 14:27:28 +0900

    Rename `memo_app.js` to `app.js`

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import minimist from "minimist";
-import { App } from "./lib/memo_app.js";
+import { App } from "./lib/app.js";
 
 const params = minimist(process.argv.slice(2));
 const app = new App();
